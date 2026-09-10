a6871d859ff1277695d40ffd5de1d03e2c52955c
PastaHubRep <1kagdc@gmail.com>
2026-09-10 13:30:02 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2,7 +2,6 @@ local cloneref = (cloneref or clonereference or function(instance: any)
     return instance
 end)
 local CoreGui: CoreGui = cloneref(game:GetService("CoreGui"))
-local GuiService: GuiService = cloneref(game:GetService("GuiService"))
 local Players: Players = cloneref(game:GetService("Players"))
 local RunService: RunService = cloneref(game:GetService("RunService"))
 local SoundService: SoundService = cloneref(game:GetService("SoundService"))
